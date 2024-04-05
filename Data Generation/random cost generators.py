95237e6475796b62a387cb0bc4aa5ce7a9e64be2
Peter Roberts <65875404+petermroberts@users.noreply.github.com>
2024-04-05 13:15:16 -0300

    so laptop can access

---
+++ Data Generation/random cost generators.py
@@ -0,0 +1,13 @@
+'''
+Created on: Monday, March 25th, 2024
+@author: Peter Roberts
+
+This program will generate a random cost for a given category given a criteria.
+
+The categories this program will focus primarily on are Food & Drink
+'''
+
+import random
+
+
+def random_hours
